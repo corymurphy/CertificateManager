dac67104dcca75d26ee7a0f8680fac8ccc30329f
Cory Murphy <cmurphy@muellerflo.com>
2017-10-09 15:30:15 -0400

    Ignored node modules

--- CertificateManager/gulpfile.js
+++ CertificateManager/gulpfile.js
@@ -3,6 +3,10 @@ var gulp = require("gulp"),
     fs = require("fs"),
     less = require("gulp-less");
 
+
+var sourcemaps = require('gulp-sourcemaps');
+
+
 var concat = require('gulp-concat');
 
 
@@ -17,24 +21,26 @@ gulp.task("less", function () {
 });
 
 
-var cssSrc = [
-    "node_modules/bootstrap/dist/css/bootstrap.css",
-    //"node_modules/sb_admin/css/sb-admin.css",
-    //"node_modules/morris.js/morris.css",
-    'wwwroot/css/font-awesome.css',
-    "node_modules/jsgrid/dist/jsgrid.min.css",
-    "node_modules/jsgrid/dist/jsgrid-theme.min.css",
-    "style/cm.css"
-    //"node_modules/datatables/media/css/jquery.dataTables.min.css"
-]
+//var cssSrc = [
+//    "node_modules/bootstrap/dist/css/bootstrap.css",
+//    "node_modules/sb_admin/css/sb-admin.css",
+//    //"node_modules/morris.js/morris.css",
+//    'wwwroot/css/font-awesome.css',
+//    "node_modules/jsgrid/dist/jsgrid.min.css",
+//    "node_modules/jsgrid/dist/jsgrid-theme.min.css",
+//    "style/cm.css"
+//    //"node_modules/datatables/media/css/jquery.dataTables.min.css"
+//];
 
 
 
 
-gulp.task("copy-css", function () {
-    return gulp.src(cssSrc)
-        //.pipe(less())
-        .pipe(concat('base.min.css'))
-        //.pipe(cleanCSS())
-        .pipe(gulp.dest('wwwroot/css'));
-});
+//gulp.task("copy-css", function () {
+//    return gulp.src(cssSrc)
+//        .pipe(sourcemaps.init())
+//        //.pipe(less())
+//        .pipe(concat('base.min.css'))
+//        .pipe(sourcemaps.write('.'))
+//        //.pipe(cleanCSS())
+//        .pipe(gulp.dest('wwwroot/css'));
+//});
