1bdc92fe391aa0c5e839974ee1adfd24d6d02678
Cory Murphy <cmurphy@muellerflo.com>
2017-11-30 15:59:57 -0500

    improved authorization

--- CertificateManager.UnitTests/UserManagementLogicTests.cs
+++ CertificateManager.UnitTests/UserManagementLogicTests.cs
@@ -1,7 +1,10 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using CertificateManager.Entities;
+using CertificateManager.Entities.Exceptions;
+using CertificateManager.Logic;
 using CertificateManager.Repository;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
+using System;
 
 namespace CertificateManager.UnitTests
 {
@@ -9,9 +12,38 @@ namespace CertificateManager.UnitTests
     public class UserManagementLogicTests
     {
         [TestMethod]
+        [ExpectedException(typeof(ReferencedObjectDoesNotExistException))]
         public void UserManagementLogic_SetUser_InvalidUser_ThrowsReferencedObjectDoesNotExistException()
         {
+            UpdateUserModel updateUserModel = new UpdateUserModel(){ Id = Guid.NewGuid() };
+            
+
             Mock<IConfigurationRepository> configurationRepository = new Mock<IConfigurationRepository>();
+            configurationRepository.Setup(x => x.GetAuthenticablePrincipal<AuthenticablePrincipal>(It.IsAny<Guid>())).Returns((AuthenticablePrincipal)null);
+
+
+            UserManagementLogic userManagementLogic = new UserManagementLogic(configurationRepository.Object);
+
+
+            userManagementLogic.SetUser(updateUserModel);
+
+        }
+
+
+        [TestMethod]
+        [ExpectedException(typeof(InsufficientDataException))]
+        public void UserManagementLogic_SetUser_UpdateModelHasNullUpn_ThrowsInsufficientDataException()
+        {
+            UpdateUserModel updateUserModel = new UpdateUserModel() { Id = Guid.NewGuid(), Name = null };
+            AuthenticablePrincipal validUser = new AuthenticablePrincipal();
+
+            Mock<IConfigurationRepository> configurationRepository = new Mock<IConfigurationRepository>();
+            configurationRepository.Setup(x => x.GetAuthenticablePrincipal<AuthenticablePrincipal>(It.IsAny<Guid>())).Returns(validUser);
+
+            UserManagementLogic userManagementLogic = new UserManagementLogic(configurationRepository.Object);
+
+            userManagementLogic.SetUser(updateUserModel);
+
         }
     }
 }
