9f0bd79735e31e65dd17ea64fb1563b553de9b27
Cory Murphy <cmurphy2266@gmail.com>
2018-04-15 16:57:27 -0400

    deployment automation

--- Deployment/Deploy-CertificateManager.ps1
+++ Deployment/Deploy-CertificateManager.ps1
@@ -13,7 +13,7 @@ function Get-SetCertificateManagerAclScript
         {
             throw 'Install path not found';
         }       
-
+        
         $inheritance = ([System.Security.AccessControl.InheritanceFlags]::ContainerInherit -bor [System.Security.AccessControl.InheritanceFlags]::ObjectInherit);
 
         $systemFullControlAceArgs = @(
@@ -43,16 +43,57 @@ function Get-SetCertificateManagerAclScript
         )
         $appPoolReadExecuteAce = New-Object -TypeName:'System.Security.AccessControl.FileSystemAccessRule' -ArgumentList:$appPoolReadExecuteAceArgs
 
+        $appPoolWriteAceArgs = @(
+            (New-Object -TypeName:'System.Security.Principal.NTAccount' -ArgumentList:@('IIS APPPOOL\CertificateManager')),
+            [System.Security.AccessControl.FileSystemRights]::Write,
+            $inheritance,
+            [System.Security.AccessControl.PropagationFlags]::None,
+            [System.Security.AccessControl.AccessControlType]::Allow
+        )
+        $appPoolWriteAce = New-Object -TypeName:'System.Security.AccessControl.FileSystemAccessRule' -ArgumentList:$appPoolWriteAceArgs
+
+        $appPoolWriteFileAceArgs = @(
+            (New-Object -TypeName:'System.Security.Principal.NTAccount' -ArgumentList:@('IIS APPPOOL\CertificateManager')),
+            [System.Security.AccessControl.FileSystemRights]::Write,
+            [System.Security.AccessControl.InheritanceFlags]::None,
+            [System.Security.AccessControl.PropagationFlags]::None,
+            [System.Security.AccessControl.AccessControlType]::Allow
+        )
+        $appPoolWriteFileAce = New-Object -TypeName:'System.Security.AccessControl.FileSystemAccessRule' -ArgumentList:$appPoolWriteFileAceArgs
+
 
         $acl = [System.Security.AccessControl.DirectorySecurity]::new();
         
         $acl.SetOwner( (New-Object -TypeName:'System.Security.Principal.NTAccount' -ArgumentList:@('BUILTIN\Administrators')) );
 
-        $acl.AddAccessRule($adminFullControlAceArgs);
-        $acl.AddAccessRule($systemFullControlAceArgs);
+        $acl.AddAccessRule($systemFullControlAce);
+        $acl.AddAccessRule($adminFullControlAce);
         $acl.AddAccessRule($appPoolReadExecuteAce);
 
+        $acl.SetAccessRuleProtection($true, $true);
+
         Set-Acl -Path:$InstallPath -AclObject:$acl;
+
+        <#
+                Get-ChildItem -Path:$InstallPath -Filter "appSettings*.json" | ForEach-Object -Process {
+            $acl = Get-Acl -Path $_.FullName;
+            $acl.AddAccessRule($appPoolWriteFileAce);
+            $acl.SetAccessRuleProtection($true, $true);
+            Set-Acl -Path:$InstallPath -AclObject:$acl;
+        };
+
+
+        
+        #>
+
+        $dbDirectory = ( [System.IO.Path]::Combine($InstallPath, 'db') );
+        $dbAcl = Get-Acl -Path:$dbDirectory
+
+        $dbAcl.AddAccessRule($appPoolWriteAce);
+        #$dbAcl.SetAccessRuleProtection($true, $true);
+        Set-Acl -Path:$dbDirectory -AclObject:$acl;
+        
+
 
     }
     return $script;
@@ -65,13 +106,14 @@ function New-CertificateManagerIISConfiguration
     (
         $InstallPath,
         $HostName,
-        $Thumbprint
+        $Thumbprint,
+        [string]$HostingPackagePath
     )
     $start = Get-Location;
 
     Set-Location $env:TEMP | Out-Null;
 
-    $result = CertificateManagerIISConfiguration -InstallPath:$InstallPath -HostName:$HostName -Thumbprint:$Thumbprint;
+    $result = CertificateManagerIISConfiguration -InstallPath:$InstallPath -HostName:$HostName -Thumbprint:$Thumbprint -HostingPackagePath:$HostingPackagePath;
 
     Set-Location $start | Out-Null;
 
@@ -83,13 +125,17 @@ function Install-CertificateManagerRequiredModuled
 {
     param($Session)
 
-    $moduleSourcePath = [System.IO.Path]::Combine($env:TEMP, 'xWebAdministration');
+    $xWebAdministrationSourcePath = [System.IO.Path]::Combine($env:TEMP, 'xWebAdministration');
+    $xPSDesiredStateConfigurationSourcePath = [System.IO.Path]::Combine($env:TEMP, 'xPSDesiredStateConfiguration');
 
-    Save-Module -Name 'xWebAdministration' -Path $env:TEMP -Force
+    Save-Module -Name 'xWebAdministration' -Path $env:TEMP -Force;
+    Save-Module -Name 'xPSDesiredStateConfiguration' -Path $env:TEMP -Force;
 
-    Copy-Item -Path:$moduleSourcePath -Destination:'C:\Program Files\WindowsPowerShell\Modules' -ToSession:$Session -Force -Recurse;
+    Copy-Item -Path:$xWebAdministrationSourcePath -Destination:'C:\Program Files\WindowsPowerShell\Modules' -ToSession:$Session -Force -Recurse;
+    Copy-Item -Path:$xPSDesiredStateConfigurationSourcePath -Destination:'C:\Program Files\WindowsPowerShell\Modules' -ToSession:$Session -Force -Recurse;
 
-    Remove-Item -Path:$moduleSourcePath -Force -Recurse -Confirm:$false;
+    Remove-Item -Path:$xWebAdministrationSourcePath -Force -Recurse -Confirm:$false;
+    Remove-Item -Path:$xPSDesiredStateConfigurationSourcePath -Force -Recurse -Confirm:$false;
 }
 
 
@@ -100,10 +146,11 @@ configuration CertificateManagerIISConfiguration
         [String]$InstallPath,
         [string]$HostName,
         [string]$Thumbprint,
-        [string]$NodeName
+        [string]$HostingPackagePath
     )
 
     Import-DscResource -ModuleName 'PSDesiredStateConfiguration';
+    Import-DscResource -ModuleName 'xPSDesiredStateConfiguration';
     Import-DscResource -ModuleName 'xWebAdministration';
 
     WindowsFeature IIS {
@@ -124,9 +171,22 @@ configuration CertificateManagerIISConfiguration
         }
     }
 
+    Package InstallDotNetCoreWindowsHosting
+    {
+           Ensure = "Present"
+           Path = $HostingPackagePath
+           Arguments = "/q /norestart"
+           Name = "DotNetCore"
+           #52EB917D-6633-4063-BBDE-A57FA2E51F32
+           #"2E6AD27D-9060-324F-AB1B-7C0F837583B3" - package x64
+           ProductId = "52EB917D-6633-4063-BBDE-A57FA2E51F32"
+           DependsOn = '[WindowsFeature]IIS'
+    }
+
     xWebAppPool CertMgrAppPool
     {
         Name = 'CertificateManager'
+        DependsOn = '[WindowsFeature]IIS'
     }
 
     xWebsite NewWebsite
@@ -145,8 +205,9 @@ configuration CertificateManagerIISConfiguration
                              }
                          )
         ApplicationPool = 'CertificateManager'
-        #DependsOn       = "[File]WebContent"
+        DependsOn = '[WindowsFeature]IIS'
     }
+
 }
 
 function Deploy-CertificateManager
@@ -157,7 +218,8 @@ function Deploy-CertificateManager
         [Parameter(mandatory=$true)][PSCredential]$Credential,
         [Parameter(mandatory=$false)][string]$WebsiteHostname = 'certificatemanager.local',
         [Parameter(mandatory=$false)][ValidateNotNullOrEmpty()][string]$InstallPath = 'c:\Program Files\CertificateManager',
-        [Parameter(mandatory=$false)][ValidateNotNullOrEmpty()][string]$PackageUri = 'https://github.com/corymurphy/CertificateManager/blob/master/Deployment/CertificateManagerWebApp.nupkg?raw=true'
+        [Parameter(mandatory=$false)][ValidateNotNullOrEmpty()][string]$PackageUri = 'https://github.com/corymurphy/CertificateManager/blob/master/Deployment/CertificateManagerWebApp.nupkg?raw=true',
+        [Parameter(mandatory=$false)][ValidateNotNullOrEmpty()][string]$DotNetCoreHostingPackageUri = 'https://download.microsoft.com/download/8/D/A/8DA04DA7-565B-4372-BBCE-D44C7809A467/DotNetCore.2.0.6-1-WindowsHosting.exe'
     )
 
     begin
@@ -165,22 +227,31 @@ function Deploy-CertificateManager
         [Net.ServicePointManager]::SecurityProtocol = [Net.SecurityProtocolType]::Tls12;
         $packageName = 'CertificateManagerWebApp.nupkg';
         $packageZipName = 'CertificateManagerWebApp.zip';
+        $hostingPackageName = 'DotNetCore.2.0.6-1-WindowsHosting.exe';
         $packagePath = [System.IO.Path]::Combine($env:TEMP, $packageName);
         $serverTempDirectory = 'c:\windows\temp';
         $ConfigurationName = 'CertificateManagerIISConfiguration';
         $serverConfigPath = ([IO.Path]::Combine($serverTempDirectory, $ConfigurationName));
+        $hostingBundlePath = [System.IO.Path]::Combine($env:TEMP, $hostingPackageName);
 
         $downloadArgs = @{
             Uri = $PackageUri;
             OutFile = $packagePath;
         };
 
+
+        $downloadDotNetCoreHostingArgs = @{
+            Uri = $DotNetCoreHostingPackageUri;
+            OutFile = $hostingBundlePath;
+        }
+
         $newCertScript = {
             param($HostName)
             New-SelfSignedCertificate -DnsName:$HostName;
         }
 
-        Install-Module -Name 'xWebAdministration' -Scope CurrentUser -ErrorAction:Stop;
+        Install-Module -Name 'xWebAdministration' -Scope CurrentUser -Confirm:$false -ErrorAction:Stop -WarningAction:SilentlyContinue;
+        Install-Module -Name 'xPSDesiredStateConfiguration' -Scope CurrentUser -Confirm:$false -ErrorAction:Stop -WarningAction:SilentlyContinue;
 
         $setupScript = { 
             param
@@ -193,14 +264,17 @@ function Deploy-CertificateManager
                 $WebsiteHostname
             )
 
+            
             $PackagePath = [IO.Path]::Combine($ServerTempDirectory, $PackageName);
             $PackageZipPath = [IO.Path]::Combine($ServerTempDirectory, $PackageZipName);
 
-            Remove-Item -Path $PackageZipPath -Force;
-            Rename-Item -Path $PackagePath -NewName $PackageZipName -Force;
+            Remove-Item -Path $PackageZipPath -Force -ErrorAction SilentlyContinue;
+            Rename-Item -Path $PackagePath -NewName $PackageZipName -Force -ErrorAction SilentlyContinue;
             Expand-Archive -Path $PackageZipPath -DestinationPath $InstallPath -Force;
 
-            $job = Start-DscConfiguration $ConfigurationPath;
+            New-Item -Path:$InstallPath -Name 'db' -ItemType:'Directory' -Force | Out-Null;
+
+            $job = Start-DscConfiguration -Path:$ConfigurationPath -Force;
 
             $StopWatch = New-Object -TypeName System.Diagnostics.Stopwatch
 
@@ -217,12 +291,13 @@ function Deploy-CertificateManager
             if( ($job | Get-Job).State -ne 'Completed' )
             {
                 $result = $job | Receive-Job;
-                throw 'IIS Configuration failed {0}{1}' -f [System.Environment]::NewLine,$result;
+                throw $result;
+                #throw 'IIS Configuration failed {0}{1}' -f [System.Environment]::NewLine,$result;
             }
 
             Import-Module -Name 'WebAdministration' -ErrorAction 'Stop';
 
-            Set-ItemProperty -Path 'IIS:\AppPools\CertificateManager' -Name 'managedRuntimeVersion' -Value [string]::Empty -Force;
+            Set-ItemProperty -Path 'IIS:\AppPools\CertificateManager' -Name 'managedRuntimeVersion' -Value '' -Force;
 
             #Invoke-Command -ScriptBlock:$AclConfigScript -ArgumentList:@($InstallPath);
         }
@@ -239,13 +314,17 @@ function Deploy-CertificateManager
 
         $cert = Invoke-Command -ScriptBlock:$newCertScript -ArgumentList @($WebsiteHostname) -Session:$session;
 
-        $configurationPath = New-CertificateManagerIISConfiguration -InstallPath:$InstallPath -HostName:$WebsiteHostname -Thumbprint:$cert.Thumbprint;
+        $configurationPath = New-CertificateManagerIISConfiguration -InstallPath:$InstallPath -HostName:$WebsiteHostname -Thumbprint:$cert.Thumbprint -HostingPackagePath:( [IO.Path]::Combine($ServerTempDirectory, $hostingPackageName) );
 
         Install-CertificateManagerRequiredModuled -Session:$Session;
 
         Invoke-WebRequest @downloadArgs | Out-Null;
 
-        Copy-Item -ToSession $session -Path $packagePath -Destination $serverTempDirectory -Force;
+        Invoke-WebRequest @downloadDotNetCoreHostingArgs | Out-Null;
+
+        Copy-Item -ToSession:$session -Path:$packagePath -Destination:$serverTempDirectory -Force;
+
+        Copy-Item -ToSession:$session -Path:$hostingBundlePath -Destination:$serverTempDirectory -Force;
 
         Invoke-Command -ScriptBlock $createDscFolderScript -ArgumentList @($serverTempDirectory, $ConfigurationName) -Session $session;
 
@@ -262,7 +341,7 @@ function Deploy-CertificateManager
         
         Invoke-Command -ScriptBlock $setupScript -ArgumentList $setupArgs -Session $session;
 
-        Invoke-Command -ScriptBlock:(Get-SetCertificateManagerAclScript) -ArgumentList:@($InstallPath) -Session:$session;
+        Invoke-Command -ScriptBlock:([scriptblock](Get-SetCertificateManagerAclScript)) -ArgumentList:@($InstallPath) -Session:$session;
     }
 }
 
