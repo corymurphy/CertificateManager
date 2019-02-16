b7b87b5f9cc5fd387ab40886ef44a142a6eab8f8
Cory Murphy <cmurphy2266@gmail.com>
2019-02-15 19:27:31 -0500

    successful build

--- Deployment/Package-CertificateManager.ps1
+++ Deployment/Package-CertificateManager.ps1
@@ -1,1 +1,2 @@
 nuget pack 'CertificateManager.nuspec' -OutputFileNamesWithoutVersion -NoPackageAnalysis -OutputDirectory .
+Copy-Item -Path .\CertificateManagerWebApp.nupkg -Destination '\\srv02\repo'
